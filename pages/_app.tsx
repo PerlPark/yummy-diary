5e36855577b1d6b0864908eea419b889efa6e9ed
perlpark <hello@jinjoo.me>
2023-04-17 23:33:47 +0900

    mod: root px css 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { RecoilRoot } from 'recoil';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <div className="py-24">
+      <div className="py-24 px-5">
         <Component {...pageProps} />
       </div>
     </RecoilRoot>
