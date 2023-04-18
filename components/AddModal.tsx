ae6f7b48dc26b7d37ac01ad1631ff244e0aac7b4
perlpark <hello@jinjoo.me>
2023-04-18 22:00:45 +0900

    feat: 날짜 선택 버튼 UI 추가, CSS 수정

--- components/AddModal.tsx
+++ components/AddModal.tsx
@@ -6,6 +6,7 @@ import labels from '@/constants/labels';
 import { Data, ItemType, dataState } from '@/recoil/data';
 import { useRecoilState } from 'recoil';
 import FoodItem from './FoodItem';
+import { CalendarIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 type AddModalPropsType = {
   date: string;
@@ -54,90 +55,106 @@ const AddModal = ({ date, closeHandler }: AddModalPropsType) => {
 
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-slate-950/30 px-4 overflow-auto z-50">
-      <div className="max-w-3xl mx-auto my-12 bg-white rounded-lg shadow p-7 relative">
-        <h1 className="text-3xl font-semibold mb-3">추가하기</h1>
-        <span className="text-lg">{date}</span>
+      <div className="max-w-3xl mx-auto my-12 bg-white rounded-lg shadow relative">
+        <h1 className="text-3xl font-semibold mb-3 p-7 pb-3">추가하기</h1>
         <button
           type="button"
           onClick={closeHandler}
-          className="absolute right-0 top-0"
+          className="absolute right-4 top-4"
         >
-          닫기
+          <XMarkIcon className="w-6 h-6" />
         </button>
 
-        <div className="pb-4 mb-5">
-          {!selected && (
-            <div className="py-10 text-center">아이템을 선택해 주세요.</div>
-          )}
-          {selected && (
-            <div className="flex mt-5 gap-4">
-              <div className="w-2/5 mb-2">
-                <div
-                  className="w-full overflow-hidden relative"
-                  style={{ paddingBottom: '100%' }}
-                >
-                  <img
-                    src={selected.image}
-                    alt=""
-                    className="rounded absolute top-0 left-0 w-full h-full object-cover border bg-slate-100"
-                  />
-                </div>
-              </div>
-              <div>
-                <span className="text-sm text-gray-500">{selected.brand}</span>
-                <h3 className="text-xl font-medium mt-1 mb-6">
-                  {selected.name}
-                </h3>
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-2">
-                  {Object.keys(selected.nutrition).map((key) => (
-                    <div
-                      key={`${selected.index}-${key}`}
-                      className="flex gap-1 grow-0"
-                    >
-                      <label className="block w-20 text-sm font-medium shrink-0">
-                        {labels[key]}
-                      </label>
-                      <input
-                        type="text"
-                        defaultValue={
-                          selected.nutrition[key as keyof ItemType['nutrition']]
-                        }
-                        className="rounded bg-gray-100 px-3 py-1 text-sm flex-grow"
-                        style={{ width: 'calc(100% - 80px)' }}
-                      />
-                    </div>
-                  ))}
-                </div>
-                <div className="mt-6">
-                  <button
-                    type="button"
-                    className="border rounded w-full h-11"
-                    onClick={() => {
-                      addLog(selected);
-                      closeHandler();
-                    }}
-                  >
-                    등록하기
-                  </button>
-                </div>
-              </div>
-            </div>
-          )}
+        <div className="border-t border-b py-4 px-7 flex gap-7">
+          <button
+            type="button"
+            className="flex font-medium items-center gap-1.5"
+          >
+            <span className="">{date}</span>
+            <CalendarIcon className="h-5 w-5 text-rose-400" />
+          </button>
+          <button>오전</button>
         </div>
 
-        <h2 className="text-xl font-semibold mb-2">등록된 아이템</h2>
-        <div className="-mx-2">
-          {items.map((item) => (
-            <FoodItem
-              key={item.index}
-              name={item.name}
-              brand={item.brand}
-              image={item.image}
-              onClickHandler={() => setSelected(item)}
-            />
-          ))}
+        <div className="px-7 py-3">
+          <div className="pb-4 mb-5">
+            {!selected && (
+              <div className="py-10 text-center">아이템을 선택해 주세요.</div>
+            )}
+            {selected && (
+              <div className="flex mt-5 gap-4">
+                <div className="w-2/5 mb-2">
+                  <div
+                    className="w-full overflow-hidden relative"
+                    style={{ paddingBottom: '100%' }}
+                  >
+                    <img
+                      src={selected.image}
+                      alt=""
+                      className="rounded absolute top-0 left-0 w-full h-full object-cover border bg-slate-100"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <span className="text-sm text-gray-500">
+                    {selected.brand}
+                  </span>
+                  <h3 className="text-xl font-medium mt-1 mb-6">
+                    {selected.name}
+                  </h3>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-2">
+                    {Object.keys(selected.nutrition).map((key) => (
+                      <div
+                        key={`${selected.index}-${key}`}
+                        className="flex gap-1 grow-0"
+                      >
+                        <label className="block w-20 text-sm font-medium shrink-0">
+                          {labels[key]}
+                        </label>
+                        <input
+                          type="text"
+                          defaultValue={
+                            selected.nutrition[
+                              key as keyof ItemType['nutrition']
+                            ]
+                          }
+                          className="rounded bg-gray-100 px-3 py-1 text-sm flex-grow"
+                          style={{ width: 'calc(100% - 80px)' }}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <div className="mt-6">
+                    <button
+                      type="button"
+                      className="border rounded w-full h-11"
+                      onClick={() => {
+                        addLog(selected);
+                        closeHandler();
+                      }}
+                    >
+                      등록하기
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <h2 className="text-xl font-semibold mb-2">등록된 아이템</h2>
+          <div className="-mx-2">
+            {items.map((item) => (
+              <FoodItem
+                key={item.index}
+                name={item.name}
+                brand={item.brand}
+                image={item.image}
+                onClickHandler={() => setSelected(item)}
+              />
+            ))}
+          </div>
+          <AddButton text="새 아이템 추가하기" onClick={() => {}} />
         </div>
-        <AddButton text="새 아이템 추가하기" onClick={() => {}} />
       </div>
     </div>
   );
--- components/FoodItem.tsx
+++ components/FoodItem.tsx
@@ -21,7 +21,7 @@ const FoodItem = ({
       <img
         src={image}
         alt=""
-        className="rounded mb-2 h-40 object-cover border"
+        className="rounded mb-1.5 h-40 object-cover border"
       />
       <span className="text-xs text-gray-500">{brand}</span>
       <h3 className="text-base font-medium mt-0.5">{name}</h3>
